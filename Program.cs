3430ffe11d06c5811e3e47ecf3fd92781d43c8c6
Natália Cavalcante <nataliacavalcantebl@gmail.com>
2022-11-27 21:23:08 -0300

    Aula 02

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Aula02
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine ("Brenda Failache");
+            if(args.GetLength(0)>0){
+                Console.Write (args.GetValue(0));
+            }
+        }
+    }
+}
